c2ed81fdd9195bc3060ec10674a61058fee169a0
Matheus Araujo <matheus.kyuubinoz@gmail.com>
2022-10-25 21:34:45 -0300

    refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,40 @@
-const newLocal = "https://api.adviceslip.com/advice";
-const url = newLocal;
-const button = document.querySelector(".card-advice-button button")
+document.addEventListener('DOMContentLoaded', () => {
 
-function requestAdvice(url) {
-    fetch(url)
-        .then(response => response.json())
-        .then((data) => {
-            const divtitle = document.querySelector(".card-advice-title h1")
-            const divtext = document.querySelector(".card-advice-text")
+    const url = "https://api.adviceslip.com/advice";
+    const button = document.querySelector(".card-advice-button button")
+    const divtitle = document.querySelector(".card-advice-title h1")
+    const divtext = document.querySelector(".card-advice-text")
 
-            divtitle.innerHTML = `advice # ${data.slip.id}`
-            divtext.innerHTML = `"${data.slip.advice}"`
-        })
+    renderAdvices(url)
 
-}
-requestAdvice(url)
+    async function requestAdvice(url) {
+
+        try {
+            let data = await fetch(url)
+            let dataBody = await data.json()
+
+            return dataBody
+        } catch (error) {
+            throw new Error(error.message)
+  
+        }
+
+    }
+
+    async function renderAdvices() {
+        let advice = await requestAdvice(url)
+
+
+        divtitle.innerHTML = ''
+        divtext.innerHTML = ''
+
+        divtitle.innerHTML = `advice # ${advice.slip.id}`
+        divtext.innerHTML = `"${advice.slip.advice}"`
+
+    }
+
+    button.addEventListener('click', () => {
+        renderAdvices()
+    })
+
+})
